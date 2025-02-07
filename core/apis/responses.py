6a9013d5b33ac11415ae3552256c89fa06a82251
Krithick Jegatheesan <j.krithick@gmail.com>
2025-02-08 02:30:24 +0530

    Revert "resubmit assignment test case pass"
    This reverts commit 7f5997bb0c38e239bc7baf441b95a0c1817f4253.

--- core/apis/responses.py
+++ core/apis/responses.py
@@ -6,7 +6,3 @@ class APIResponse(Response):
     @classmethod
     def respond(cls, data, status_code=HTTPStatus.OK):
         return make_response(jsonify(data=data), status_code)
-
-    @classmethod
-    def respond_error(cls, message, error, status_code=HTTPStatus.BAD_REQUEST):
-        return make_response(jsonify(message=message, error=error), status_code)
